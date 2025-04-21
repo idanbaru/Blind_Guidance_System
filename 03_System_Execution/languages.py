170cd8a298e5aa693734a1f554ca50a80f138875
idanb <itai.benyamin@gmail.com>
2025-04-21 13:25:29 +0300

    Add language pack file for future implementation

---
+++ 03_System_Execution/languages.py
@@ -0,0 +1,33 @@
+english = {
+    "SNAPSHOT_CAPTIONING_PROMPT": "In a short sentence, please describe the image to a blind person.",
+    "I_SEE": "I see: ",
+    "AND": " and ",
+    "DIRECTION_LEFT": "to the left",
+    "DIRECTION_MIDDLE": "in front",
+    "DIRECTION_RIGHT": "to the right",
+    "OBJECT_AT": "{label} in {distance:.2f} meters {direction}",
+    "ATTENTION": "Attention! {label} coming from the {direction} in {distance:.2f} meters."
+}
+
+hebrew = {
+    "SNAPSHOT_CAPTIONING_PROMPT": "במשפט קצר, תאר את התמונה לאדם עיוור",
+    "I_SEE": "אני רואה: ",
+    "AND": " ו",
+    "DIRECTION_LEFT": "משמאל",
+    "DIRECTION_MIDDLE": "מלפנים",
+    "DIRECTION_RIGHT": "מימין",
+    "OBJECT_AT": "{label} במרחק {distance:.2f} מטר {direction}",
+    "ATTENTION": "שימו לב! {label} מתקרב {direction} במרחק {distance:.2f} מטר."
+}
+
+german = {
+    "SNAPSHOT_CAPTIONING_PROMPT": "",
+    "I_SEE": "Ich sehe:",
+    "AND": " und",
+    "DIRECTION_LEFT": "links",
+    "DIRECTION_MIDDLE": "rechts",
+    "DIRECTION_RIGHT": "davor",
+    "OBJECT_AT": "{direction} {label} in {distance:.2f} meter ",
+    "ATTENTION": " {label}  {direction}  {distance:.2f} "
+}
+
